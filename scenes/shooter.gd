c83b0e9fcb96749c72863fc9f389b65358eaa50a
Karidus-423 <kapuerto23@gmail.com>
2025-08-14 21:47:37 -0400

    understanding callbacks and setup window ratio to keep

--- scenes/shooter.gd
+++ scenes/shooter.gd
@@ -1,14 +1,18 @@
 extends Node2D
 
-var chucki_scene = preload("res://scenes/chuki.tscn")
+var chuki_scene = preload("res://scenes/chuki.tscn")
 
-@onready var shooter_pos = $root/shooter
 
-func _ready() -> void:
-	print("From: ", shooter_pos)
+# func _ready() -> void:
 
 func _input(event: InputEvent) -> void:
-	var chuki_instance = chucki_scene.instantiate()
+	var chuki_instance = chuki_scene.instantiate()
+
 	if Input.is_action_pressed("shoot"):
 		add_child(chuki_instance)
 		print("To: ",event.position)
+
+
+func _on_child_entered_tree(node: Node) -> void:
+	var pos = node.position
+	print("From :", pos)
